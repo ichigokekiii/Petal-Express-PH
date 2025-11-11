9542c7b068b265f8c62f4cc6069bf7cdfc06086a
Cake <90282274+ichigokekiii@users.noreply.github.com>
2025-11-12 05:42:03 +0800

    Database Connection
    Stepping stone of remiel pinga

---
+++ Models/Context/PetalExpressContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Petal_Express_PH.Models.Context
+{
+    public class PetalExpressContext : DbContext
+    {
+
+        static PetalExpressContext()
+        {
+            Database.SetInitializer<PetalExpressContext>(null);
+        }
+
+        public PetalExpressContext() : base("Name=3ite_db") {}
+    }
+}
---
+++ Models/tblRegistrationsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Petal_Express_PH.Models
+{
+    public class tblRegistrationsModel
+    {
+        public int registrationID { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public DateTime createdAt { get; set; }
+
+        public DateTime updatedAt { get; set; }
+    }
+}
